ef42b4f503c6e4d7aa30d86ab30b491f4fe1fd70
Aaron Lin <lin.aaron1@gmail.com>
2015-06-18 17:16:26 +0300

    functional set game for 12 cards

--- mechanics.js
+++ mechanics.js
@@ -1,8 +1,12 @@
 
 
-
+var ALPHABET = 'abcdefghijklmnopqrstu';
 var g_cardsDealt = 0;
-var g_cardsList;
+var g_cardSequence;
+var g_tilesSelected = [];
+var g_cardsOnBoard = new Array(21);
+var g_numSelected = 0;
+var g_vNum = 12;
 
 function loadNewCards() {
 	var cards = new Array();
@@ -25,23 +29,94 @@ function loadNewCards() {
 	return cards;
 }
 
+function isSet(cardNos) {
+	card = new Array(3);
+	mod = new Array(3);
+	for(var i=0; i<3; i++) {
+		card[i] = parseInt(cardNos[i])-1;
+	}
+
+	while(!(card[0]==0 && card[1]==0 && card[2]==0)) {
+		for(var i=0; i<3; i++) {
+			mod[i] = card[i]%3;
+			card[i] = (card[i]-mod[i])/3;
+		}
+		if ((mod[0] + mod[1] + mod[2])%3!=0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function deal_three_more(tile_list) {
+	for(var i=0; i<3; i++) {
+		deal_card(tile_list[i]);
+	}
+}
+
+function deal_card(tile) {
+	img_path = "url(./cardimgs/"+g_cardSequence[g_cardsDealt]+".gif)";
+	$("#card_"+tile).css("background-image", img_path);
+	g_cardsOnBoard[tileToInt(tile)] = g_cardSequence[g_cardsDealt];
+	g_cardsDealt++;
+}
+
+function tileToInt(tile) {
+	return tile.charCodeAt() - 'a'.charCodeAt();
+}
+
 $(document).ready(function() {
 	$("#thebutton").click(function() {
 		g_cardsDealt++;
 		alert(g_cardsDealt);
 	});
+
 	$("#startGame").click(function() {
 		g_cardsDealt = 0;
-		g_cardsList = loadNewCards();
+		g_cardSequence = loadNewCards();
 
-		var alphabet = "abcdefghijkl";
-		alphabet = alphabet.split("");
+		alphabet = ALPHABET.substring(0,g_vNum).split("");
 		for(var i=0; i<alphabet.length; i++) {
-			img_path = "url(./cardimgs/"+g_cardsList[g_cardsDealt]+".gif)";
-			td_selector = $("#card_"+alphabet[i]);
-			g_cardsDealt++;
+			deal_card(alphabet[i]);
+		}
+	});
 
-			td_selector.css("background-image", img_path);
+	$(document).keypress(function(press) {
+		key = String.fromCharCode(press.which).toLowerCase();
+		if (ALPHABET.substring(0,g_vNum).indexOf(key) != -1) {
+			selected = $("#card_"+key);
+			selected.toggleClass("selected_card");
+			var bool_selected = selected.hasClass("selected_card");
+
+
+			if(g_tilesSelected.length<3) {
+				index = g_tilesSelected.indexOf(key)
+				if(index != -1) {
+					g_tilesSelected.splice(index,1);
+				}
+				else {
+					g_tilesSelected.push(key);
+				}
+			}
+			
+			if(g_tilesSelected.length==3) {
+				// Unselect everything; this is fragile
+				cardNos = [];
+				for(var i=0; i<3; i++) {
+					tile = g_tilesSelected[i];
+					$("#card_"+tile).toggleClass("selected_card");
+					cardNos.push(g_cardsOnBoard[tileToInt(tile)]);
+				}
+
+				if (isSet(cardNos)) {
+					deal_three_more(g_tilesSelected);
+				}
+				else {
+					// not a set
+				}
+
+				g_tilesSelected = [];
+			}
 		}
 	});
 });
