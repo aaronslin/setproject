f1bb952926f6a4ee2727184dd306f361329633e3
Aaron Lin <lin.aaron1@gmail.com>
2016-09-14 12:10:04 -0400

    account registration (currently broken

--- backend.js
+++ backend.js
@@ -1,23 +1,53 @@
 // backend
 
 // Goal 1: Add user accounts?
+//			Find a way to regi
 
-var myFirebaseRef = new Firebase("https://setproject-9792a.firebaseio.com/");
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDZnCVGk4o-A3YGCCjVA8u4iBJuG7L98es",
+	authDomain: "setproject-9792a.firebaseapp.com",
+	databaseURL: "https://setproject-9792a.firebaseio.com",
+	storageBucket: "setproject-9792a.appspot.com",
+};
+firebase.initializeApp(config);
+
+var firebaseRef = new firebase.database().ref();
 
 
+function createUser(userObj) {
+	firebaseRef.createUser(userObj, function(error, userData) {
+		if(error) {
+			console.log("Error creating user:", error);
+		} else {
+			console.log("Successfully created account:", userData);
+		}
+	});
+}
 
+$("#regForm").on("submit", function(event) {
+	event.preventDefault();
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-  } else {
-    // No user is signed in.
-  }
+	userObj = {};
+	$(this).serializeArray().map(function(x) {
+		userObj[x.name] = x.value;
+	});
+	createUser(userObj);
 });
 
 
 
-// pseudocode
-$("login_button").on("submit", function {
+// firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     // User is signed in.
+//   } else {
+//     // No user is signed in.
+//   }
+// });
 
-});
+
+
+// // pseudocode
+// $("login_button").on("submit", function {
+
+// });
